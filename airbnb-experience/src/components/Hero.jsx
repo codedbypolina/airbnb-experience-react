6d9404daf37e4ecf6e446992abf82383b0c35b4f
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-02-15 22:22:27 -0500

    Style Main Content

--- airbnb-experience/src/components/Hero.jsx
+++ airbnb-experience/src/components/Hero.jsx
@@ -1,16 +1,16 @@
 import photos from "../images/photos.png";
 
-export default function Main() {
+export default function Hero() {
   return (
-    <div className="main">
+    <section className="hero">
       <img src={photos} className="photos" />
       <div className="description">
-        <h1>Online Experiences</h1>
-        <p>
+        <h1 className="hero-heading">Online Experiences</h1>
+        <p className="hero-text">
           Join unique interactive activities led by one-of-a-kind hosts—all
           without leaving home.
         </p>
       </div>
-    </div>
+    </section>
   );
 }
