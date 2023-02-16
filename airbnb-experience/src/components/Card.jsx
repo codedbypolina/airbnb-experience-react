c8440e3514a87326c626d8b70e5ee3b20c1952ad
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-02-15 22:36:17 -0500

    Creating JSX of Card component

---
+++ airbnb-experience/src/components/Card.jsx
@@ -0,0 +1,25 @@
+import katie from "../images/katie-zaferes.png";
+import star from "../images/star.png";
+
+export default function Card() {
+  return (
+    <div className="card">
+      <img
+        src={katie}
+        className="card-image"
+        alt="Katie Zaferes"
+        width="400px"
+      />
+      <div className="card-description">
+        <img src={star} alt="Star" />
+        <span>
+          5.0 <span className="gray">(6) USA</span>{" "}
+        </span>
+        <p>Life lessons with Katie Zaferes</p>
+        <p>
+          <span className="bold">From $136</span> / person
+        </p>
+      </div>
+    </div>
+  );
+}
